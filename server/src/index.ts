4db71b3a794f3454f85002af24d59879ea550b43
Marina Safwat <66208355+marinaSwadee@users.noreply.github.com>
2024-11-28 17:41:00 +0200

    fix bug double listen on port

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,19 +2,25 @@ import express, { Application, Request, Response } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes";
-import cors from 'cors';
-
+import cors from "cors";
 
 const app: Application = express();
 dotenv.config();
+app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URL = process.env.MONGO_URL || "your-default-mongo-url";
-
-app.use(cors());
+const MONGO_URL = process.env.MONGO_URL!;
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Database connection failed:", error);
+  });
 
 // Middleware
-app.use(express.json());
 
 // ... after app initialization
 app.use("/users", userRoutes);
@@ -23,18 +29,6 @@ app.use("/users", userRoutes);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
-
-mongoose
-  .connect(MONGO_URL)
-  .then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Database connection failed:", error);
-  });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
